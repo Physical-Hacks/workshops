eaaaa8ca52444375029e5fd825195b0deb707e53
Ruairí <ruairibrett@gmail.com>
2018-11-16 13:05:53 -0500

    example python added (just the import syntax essentially..)

---
+++ pybind/complex/testBinding.py
@@ -0,0 +1,6 @@
+import examplebind as test
+
+a = test.Complex(1,2)
+
+print(a.imagPart())
+print(test.add(a,a))
---
+++ pybind/vector2/testBinding.py
@@ -0,0 +1,9 @@
+import testVector2 as test
+
+a = test.Vector2(1.0,2.0)
+b = test.Vector2(5.25,9.0)
+
+print(a)
+print(b)
+
+print(a+b)
